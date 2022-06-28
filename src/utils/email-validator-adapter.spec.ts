cdfb1fdaf450a5d16c539a834c703ea9ee803c06
Igor Monteiro <igornathandev@gmail.com>
2022-06-28 11:27:56 -0300

    refactor: move sut creation to a factory helper method

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,14 +1,18 @@
 import { EmailValidatorAdapter } from './email-validator'
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('EmailValidator Adapter', () => {
   test('should return false if validator returns false ', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('invalid_email.com')
     expect(isValid).toBe(false)
   })
 
   test('should return true if validator returns true ', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(true)
   })
