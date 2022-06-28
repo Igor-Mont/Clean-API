2a31bbc85f58562a61842c4a74049ce42e163044
Igor Monteiro <igornathandev@gmail.com>
2022-06-28 15:02:26 -0300

    test: ensure DbAddAccount return an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -96,4 +96,20 @@ describe('DbAddAccount Usecase', () => {
       password: 'hashed_password'
     })
   })
+
+  test('should return an account if on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid@email.com',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid@email.com',
+      password: 'hashed_password'
+    })
+  })
 })
