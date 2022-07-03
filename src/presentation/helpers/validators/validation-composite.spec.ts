06e10521c713d666e6709c82aa69f334d1146c0e
Igor Monteiro <igornathandev@gmail.com>
2022-07-03 14:35:04 -0300

    test: ensure ValidationComposite returns an error if any validation fails

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,25 @@
+import { MissingParamError } from '../../errors'
+import { Validation } from './validation'
+import { ValidationComposite } from './validation-composite'
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return new MissingParamError('field')
+    }
+  }
+  return new ValidationStub()
+}
+
+const makeSut = (): ValidationComposite => {
+  const validationStub = makeValidation()
+  return new ValidationComposite([validationStub])
+}
+
+describe('Validation Composite', () => {
+  test('should return an error if any validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+})
