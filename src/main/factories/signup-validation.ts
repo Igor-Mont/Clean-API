502def86a5a8fc096c28c45990d47d209f93984f
Igor Monteiro <igornathandev@gmail.com>
2022-07-03 12:22:57 -0300

    refactor: move password compartion to the ValidationComposite

--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,3 +1,4 @@
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validaion'
 import { RequiredFieldsValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
@@ -7,6 +8,8 @@ const makeSignUpValidation = (): ValidationComposite => {
   for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredFieldsValidation(field))
   }
+
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
 
   return new ValidationComposite(validations)
 }
--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -72,21 +72,6 @@ const makeSut = (): SutTypes => {
 }
 
 describe('SignUp Controller', () => {
-  test('should return 400 if no passwordConfirmation fails', async () => {
-    const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'invalid_password'
-      }
-    }
-
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new InvalidParamError('passwordConfirmation')))
-  })
-
   test('should return 400 if an invalid mail is provided', async () => {
     const { sut, emailValidatorStub } = makeSut()
 
