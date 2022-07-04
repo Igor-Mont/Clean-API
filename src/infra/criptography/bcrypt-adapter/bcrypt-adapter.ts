15162d6421afa1fd10b52079bd9834986fdc730e
Igor Monteiro <igornathandev@gmail.com>
2022-07-03 22:12:16 -0300

    refactor: move bcrypt-adapter files to a folder

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt'
-import { HashCompare } from '../../data/protocols/criptography/hash-compare'
-import { Hasher } from '../../data/protocols/criptography/hasher'
+import { HashCompare } from '../../../data/protocols/criptography/hash-compare'
+import { Hasher } from '../../../data/protocols/criptography/hasher'
 
 class BcryptAdapter implements Hasher, HashCompare {
   private readonly salt: number
