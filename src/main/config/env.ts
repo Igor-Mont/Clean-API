8600c4680bc650d54cdaa6ec53f7fd82081b07e6
Igor Monteiro <igornathandev@gmail.com>
2022-07-04 08:44:19 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || process.env.MONGODB_URI,
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || '<04j2022>'
 }
