d8305d539b105b1cecc0d65ac9c069af27867a27
Igor Monteiro <igornathandev@gmail.com>
2022-07-03 22:18:45 -0300

    feat: ensure JwtAdapter calls sign with correct values

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken'
+import { Encrypter } from '../../../data/protocols/criptography/encrypter'
+
+class JwtAdapter implements Encrypter {
+  private readonly secret: string
+
+  constructor (secret: string) {
+    this.secret = secret
+  }
+
+  async encrypt (value: string): Promise<string> {
+    await jwt.sign({ id: value }, this.secret)
+    return null
+  }
+}
+
+export { JwtAdapter }
